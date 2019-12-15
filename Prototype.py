f28cbd94641275703cc1a9d7ea870dfa21f11650
Lauren <c16452764@mydit.ie>
2019-12-15 22:07:13 +0000

    most up to date prototype

--- Prototype.py
+++ Prototype.py
@@ -1,23 +1,25 @@
+'''
+Lauren Byrne C16452764 FYP - Prototype
+Program that will detect a face from webcam input using Dlib HoG face detection.
+Detected face will have a green bounding box drawn around it.
+Using Dlib facial landmark detection, facial landmarks will be detected
+and drawn on face using blue circles
+
+----References----
+Face landmarks detection - Opencv with Python.
+“Face Landmarks Detection - Opencv with Python.” YouTube, 12 Mar. 2019,
+youtu.be/MrRGVOhARYY. Accessed 13 Dec. 2019.
+'''
+
 import cv2
-import numpy as np
 import dlib
 import csv
-import pandas as pd
-from imutils import face_utils
-import sys
 
 
-cap = cv2.VideoCapture(0)
-
+# dlib face detector and facial landmark detector models
 detector = dlib.get_frontal_face_detector()
 predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
 face_points = list()
-i = 0
-
-
-def midpoint(p1, p2):
-    return int((p1.x + p2.x) / 2), int((p1.y + p2.y) / 2)
-
 
 cap = cv2.VideoCapture(0)
 
@@ -25,8 +27,6 @@ while True:
     _, frame = cap.read()
 
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
-    # print('frame', frame)
 
     if cap.isOpened():
         fps = cap.get(cv2.CAP_PROP_FPS)
@@ -43,9 +43,6 @@ while True:
         cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 1)
 
         landmarks = predictor(gray, face)
-        #landmarks_array = face_utils.shape_to_np(landmarks)
-
-        print(landmarks)
 
         # mapping each facial landmark
         for n in range(0, 68):
@@ -53,26 +50,10 @@ while True:
             y = landmarks.part(n).y
             cv2.circle(frame, (x, y), 1, (255, 0, 0), -1)
 
-        RightEyeX = landmarks.part(46).x
-        RightEyeY = landmarks.part(46).y
-
-        # setting points for eye
-        left_point = (landmarks.part(36).x, landmarks.part(36).y)
-        right_point = (landmarks.part(39).x, landmarks.part(39).y)
-        center_top = midpoint(landmarks.part(37), landmarks.part(38))
-        center_bottom = midpoint(landmarks.part(41), landmarks.part(40))
-
-        # using eye points for drawing
-        hor_line = cv2.line(frame, left_point, right_point, (0, 0, 255), 1)
-        ver_line = cv2.line(frame, center_top, center_bottom, (0, 0, 255), 1)
-
     cv2.imshow('frame', frame)
 
-    start = 0
-    maxFrames = 5
-
-    if cv2.waitKey(1) & 0xFF == ord('y'):  # save on pressing 'y'
-        #cv2.imwrite('pic'+str(start)+'.jpg', frame)
+    # save on pressing 'y'
+    if cv2.waitKey(1) & 0xFF == ord('y'):
         for n in range(0, 68):
             x = landmarks.part(n).x
             y = landmarks.part(n).y
@@ -81,26 +62,16 @@ while True:
             face_points.append(x)
             face_points.append(y)
 
-        with open('test.csv', 'a', newline='') as myFile:
+        with open('tester.csv', 'a', newline='') as myFile:
             wr = csv.writer(myFile)
             wr.writerow(face_points)
 
-        start += 1
         face_points = []
 
-
-
-    #c = cv2.waitKey(1)
     if cv2.waitKey(10) == 27:
-                cap.release()
-                cv2.destroyAllWindows()
-                break
+        break
 
-    '''if c == 27:
-        cap.release()
-        cv2.destroyAllWindows()
-        break'''
+cap.release()
+cv2.destroyAllWindows()
 
 # print('landmarks:  ', face_points)
-
-
