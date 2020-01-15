1b85671867b85fb718bf764d45eb9eb1edc0779c
Lauren <c16452764@mydit.ie>
2020-01-15 15:29:48 +0000

    Creating new frame for detection of outer eye (extreme points)

--- BlinkingDetection.py
+++ BlinkingDetection.py
@@ -23,6 +23,7 @@ import cv2
 import dlib
 import csv
 from math import hypot
+import numpy as np
 
 
 cap = cv2.VideoCapture(0)
@@ -43,8 +44,8 @@ def get_blinking_ratio(eye_points, facial_landmarks):
     center_bottom = midpoint(facial_landmarks.part(eye_points[5]), facial_landmarks.part(eye_points[4]))
 
     # using eye points for drawing
-    hor_line = cv2.line(frame, left_point, right_point, (0, 0, 255), 1)
-    ver_line = cv2.line(frame, center_top, center_bottom, (0, 0, 255), 1)
+   # hor_line = cv2.line(frame, left_point, right_point, (0, 0, 255), 1)
+   # ver_line = cv2.line(frame, center_top, center_bottom, (0, 0, 255), 1)
 
     hor_line_length = hypot((left_point[0] - right_point[0]), (left_point[1] - right_point[1]))
     ver_line_length = hypot((center_top[0] - center_bottom[0]), (center_top[1] - center_bottom[1]))
@@ -93,15 +94,43 @@ while True:
         RightEyeX = landmarks.part(46).x
         RightEyeY = landmarks.part(46).y
 
+        #Track blinking
         blinking_ratio = (right_eye_ratio + left_eye_ratio)/2
-
         if blinking_ratio > 5.7:
             cv2.putText(frame, "BLINKING", (50, 150), font, 3, (255, 0, 0))
 
+
+        #Track gaze
+        left_eye_region = np.array([(landmarks.part(36).x, landmarks.part(36).y),
+                                    (landmarks.part(37).x, landmarks.part(37).y),
+                                    (landmarks.part(38).x, landmarks.part(38).y),
+                                    (landmarks.part(39).x, landmarks.part(39).y),
+                                    (landmarks.part(40).x, landmarks.part(40).y),
+                                    (landmarks.part(41).x, landmarks.part(41).y)], np.int32)
+
+        cv2.polylines(frame, [left_eye_region], True, (0, 150, 255), 2)
+
+        min_x = np.min(left_eye_region[:, 0])
+        max_x = np.max(left_eye_region[:, 0])
+        min_y = np.min(left_eye_region[:, 1])
+        max_y = np.max(left_eye_region[:, 1])
+
+        eye = frame[min_y: max_y, min_x: max_x]
+        eye = cv2.resize(eye, None, fx=5, fy=5)
+
+        #print(eye)
+
+        cv2.imshow('Eye', eye)
+
     cv2.imshow('frame', frame)
 
+    key = cv2.waitKey(1)
+    if key == 27:
+        cap.release()
+        cv2.destroyAllWindows()
+
     # save on pressing 'y'
-    if cv2.waitKey(1) & 0xFF == ord('y'):
+'''    if cv2.waitKey(1) & 0xFF == ord('y'):
         for n in range(0, 68):
             x = landmarks.part(n).x
             y = landmarks.part(n).y
@@ -122,6 +151,6 @@ while True:
                 break
 cap.release()
 cv2.destroyAllWindows()
-# print('landmarks:  ', face_points)
+# print('landmarks:  ', face_points)'''
 
 
