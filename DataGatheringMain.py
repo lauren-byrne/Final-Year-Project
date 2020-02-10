780cce131903193ee281539d78bb9de00e00b9e7
Lauren <you@example.com>
2020-02-10 12:08:26 +0000

    Beginning modularisation. Separated the gaze track

--- DataGatheringMain.py
+++ DataGatheringMain.py
@@ -30,20 +30,29 @@ import dlib
 import csv
 import math
 import numpy as np
+import pytesseract
+from scipy import stats
 from imutils import face_utils
+from imutils.object_detection import non_max_suppression
+from gazeTracking import get_eye_shape, get_white_ratio
 
 cap = cv2.VideoCapture(0)
 
 # dlib face detector and facial landmark detector models
 detector = dlib.get_frontal_face_detector()
 predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
+
+cheek_control = 0
+cheek_list = []
 face_points = list()
 capture = False
+control_mouth_length = 0
 numframes = 40
 currentframes = 0
 running = True
+_, frame = cap.read()
 
-cap = cv2.VideoCapture('videos/Q3.mp4')
+# cap = cv2.VideoCapture('videos/Q3.mp4')
 font = cv2.FONT_HERSHEY_SIMPLEX
 
 
@@ -52,10 +61,10 @@ def midpoint(p1, p2):
     return int((p1.x + p2.x) / 2), int((p1.y + p2.y) / 2)
 
 
-#function to caculate the rate at which an eye will blink
+# function to caculate the rate at which an eye will blink
 def get_blinking_ratio(eye_points, facial_landmarks):
 
-    #calculating the central vertical, and central horizontal lines in an eye
+    # calculating the central vertical, and central horizontal lines in an eye
     left_point = (facial_landmarks.part(eye_points[0]).x, facial_landmarks.part(eye_points[0]).y)
     right_point = (facial_landmarks.part(eye_points[3]).x, facial_landmarks.part(eye_points[3]).y)
     center_top = midpoint(facial_landmarks.part(eye_points[1]), facial_landmarks.part(eye_points[2]))
@@ -76,39 +85,37 @@ def get_blinking_ratio(eye_points, facial_landmarks):
 
     return ratio
 
+def get_eyebrow_ratio(eyebrow_points, facial_landmarks):
+    lower_left_point = (facial_landmarks.part(eyebrow_points[0]).x, facial_landmarks.part(eyebrow_points[0]).y)
+    lower_right_point = (facial_landmarks.part(eyebrow_points[1]).x, facial_landmarks.part(eyebrow_points[1]).y)
+    center = midpoint(facial_landmarks.part(eyebrow_points[2]), facial_landmarks.part(eyebrow_points[2]))
+    upper_left_point = midpoint(facial_landmarks.part(eyebrow_points[3]), facial_landmarks.part(eyebrow_points[2]))
+    upper_right_point = midpoint(facial_landmarks.part(eyebrow_points[4]), facial_landmarks.part(eyebrow_points[2]))
 
-#function that return the extreme points of the outline of an eye
-#will also draw outline on new black frame
-def get_eye_shape(eye_points, facial_landmarks, black_image):
+    dist1 = math.sqrt(((lower_left_point[0] - center[0]) ** 2) + ((lower_left_point[1] - center[1]) ** 2))
+    dist2 = math.sqrt(((lower_right_point[0] - center[0]) ** 2) + ((lower_right_point[1] - center[1]) ** 2))
+    ratio = dist1/dist2
 
-    eye_region = np.array([(facial_landmarks.part(eye_points[0]).x, facial_landmarks.part(eye_points[0]).y),
-                           (facial_landmarks.part(eye_points[1]).x, facial_landmarks.part(eye_points[1]).y),
-                           (facial_landmarks.part(eye_points[2]).x, facial_landmarks.part(eye_points[2]).y),
-                           (facial_landmarks.part(eye_points[3]).x, facial_landmarks.part(eye_points[3]).y),
-                           (facial_landmarks.part(eye_points[4]).x, facial_landmarks.part(eye_points[4]).y),
-                           (facial_landmarks.part(eye_points[5]).x, facial_landmarks.part(eye_points[5]).y)], np.int32)
+    dist3 = math.sqrt(((upper_left_point[0] - center[0]) ** 2) + ((upper_left_point[1] - center[1]) ** 2))
+    dist4 = math.sqrt(((upper_right_point[0] - center[0]) ** 2) + ((upper_right_point[1] - center[1]) ** 2))
+    ratio2 = dist3/dist4
 
-    #calculating the extreme points
-    min_x = np.min(eye_region[:, 0])
-    max_x = np.max(eye_region[:, 0])
-    min_y = np.min(eye_region[:, 1])
-    max_y = np.max(eye_region[:, 1])
-
-    cv2.polylines(black_image, [eye_region], True, 255, 2)
-    cv2.fillPoly(black_image, [eye_region], 255)
-
-    return min_x, max_x, min_y, max_y, black_image
+    return ratio, ratio2
 
 
 while True:
     _, frame = cap.read()
 
-    frame = cv2.resize(frame, None, fx=0.3, fy=0.3, interpolation=cv2.INTER_LINEAR)
-    #frame = cv2.rotate(frame, cv2.ROTATE_90_CLOCKWISE)
+    # frame = cv2.resize(frame, None, fx=0.3, fy=0.3, interpolation=cv2.INTER_LINEAR)
+    # frame = cv2.rotate(frame, cv2.ROTATE_90_CLOCKWISE)
+
+    # img = cv2.resize(frame, (0, 0), fx=0.7, fy=0.7)
+
+    height = np.size(frame, 0)
+    width = np.size(frame, 1)
 
     # creating a grayscale image
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
 
     if cap.isOpened():
         fps = cap.get(cv2.CAP_PROP_FPS)
@@ -135,7 +142,7 @@ while True:
         for n in range(0, 68):
             x = landmarks.part(n).x
             y = landmarks.part(n).y
-            # cv2.circle(frame, (x, y), 1, (255, 0, 0), -1)
+            cv2.circle(frame, (x, y), 1, (255, 0, 0), -1)
 
         # RightEyeX = landmarks.part(46).x
         # RightEyeY = landmarks.part(46).y
@@ -145,15 +152,14 @@ while True:
         left_eye_ratio = get_blinking_ratio([36, 37, 38, 39, 40, 41], landmarks)
         right_eye_ratio = get_blinking_ratio([42, 43, 44, 45, 46, 47], landmarks)
 
+
+
         # Track blinking
         blinking_ratio = (right_eye_ratio + left_eye_ratio) / 2
         # cv2.putText(frame, str(blinking_ratio), (50, 150), font, 3, (255, 0, 0))
 
         if blinking_ratio > 4.5:
             cv2.putText(frame, "BLINKING", (50, 150), font, 3, (255, 0, 0))
-
-        height = np.size(frame, 0)
-        width = np.size(frame, 1)
 
         # creating new black frame that is same size and width as original frame
         black_img = np.zeros((height, width), dtype="uint8")
@@ -164,47 +170,26 @@ while True:
         min_x_right, max_x_right, min_y_right, max_y_right, black_img = get_eye_shape([42, 43, 44, 45, 46, 47],
                                                                                       landmarks, black_img)
 
-        masked_eye = cv2.bitwise_and(frame, frame, mask=black_img)
+        left_ratio = get_white_ratio(min_x_left, max_x_left, min_y_left, max_y_left, black_img, frame)
+        right_ratio = get_white_ratio(min_x_right, max_x_right, min_y_right, max_y_right, black_img, frame)
 
-        # cropping the eye at the extreme points from the masked image - any skin around eye will now be black
-        eye = masked_eye[min_y_left: max_y_left, min_x_left: max_x_left]
-        eye = masked_eye[min_y_right: max_y_right, min_x_right: max_x_right]
-        eye = cv2.resize(eye, None, fx=5, fy=5)
+        # cv2.imshow('Eye', eye)
 
-        cv2.imshow('Eye', eye)
+        gaze_ratio = (left_ratio + right_ratio) / 2
 
-        # creating threshold for eye
-        gray_eye = cv2.cvtColor(eye, cv2.COLOR_BGR2GRAY)
-        T, B = cv2.threshold(gray_eye, 50, maxval=255, type=cv2.THRESH_BINARY)
-
-        eye_height = np.size(gray_eye, 0)
-        eye_width = np.size(gray_eye, 1)
-
-        # splitting the left eye in left and right sides
-        left_white_eye_L = gray_eye[0:eye_height, 0:int(eye_width / 2)]
-        right_white_eye_L = gray_eye[0:eye_height, int(eye_width / 2):eye_width]
-
-        # splitting the right eye in left and right sides
-        left_white_eye_R = gray_eye[0:eye_height, 0:int(eye_width / 2)]
-        right_white_eye_R = gray_eye[0:eye_height, int(eye_width / 2):eye_width]
-
-        # counting the amount of black (pupil) that appears in each side of each eye
-        left_white_amount_L = cv2.countNonZero(left_white_eye_L)
-        right_white_amount_L = cv2.countNonZero(right_white_eye_L)
-        left_white_amount_R = cv2.countNonZero(left_white_eye_R)
-        right_white_amount_R = cv2.countNonZero(right_white_eye_R)
-
-        white_ratio_L = left_white_amount_L / right_white_amount_L
-        white_ratio_R = left_white_amount_R / right_white_amount_R
-
-        gaze_ratio = (white_ratio_L + white_ratio_R) / 2
+        if gaze_ratio < 0.85:
+            cv2.putText(frame, 'LEFT', (100, 200), font, 3, (255, 0, 0))
+        if 0.85 <= gaze_ratio < 0.94:
+            cv2.putText(frame, 'CENTER', (100, 200), font, 3, (255, 0, 0))
+        if gaze_ratio >= 0.94:
+            cv2.putText(frame, 'RIGHT', (100, 200), font, 3, (255, 0, 0))
 
         #  cv2.putText(frame, str(gaze_ratio), (50, 150), font, 3, (255, 0, 0))
 
-        cv2.imshow('Black Frame', black_img)
-        cv2.imshow('Thresh Eye', B)
+        # cv2.imshow('Black Frame', black_img)
+        #cv2.imshow('Thresh Eye', B)
 
-        cv2.imshow('masked result', masked_eye)
+        # cv2.imshow('masked result', masked_eye)
 
         landmarks2 = face_utils.shape_to_np(landmarks)
 
@@ -214,18 +199,76 @@ while True:
         crop = cv2.cvtColor(crop, cv2.COLOR_BGR2GRAY)
         crop2 = cv2.cvtColor(crop2, cv2.COLOR_BGR2GRAY)
 
-        cv2.imshow('right cheek', crop)
-        cv2.imshow('left cheek', crop2)
+        # cv2.imshow('right cheek', crop)
+        # cv2.imshow('left cheek', crop2)
 
         cheek_average = (np.mean(crop) + np.mean(crop2))
 
-        print('average cheek: ', cheek_average)
+        cheek_list.append(cheek_average)
+        cheek_array = mat = np.array(cheek_list)
+        # print('cheek list: ', cheek_list)
+
+        if cheek_control == 0:
+            cheek_control = cheek_average
+        # print('z_score: ', stats.zscore(cheek_list))
+
+        cheek_difference = ((cheek_control - cheek_average) / cheek_control) * 100
+
+       # contours, h = cv2.findContours(B, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+       # for c in contours:
+       #     if len(contours) != 0:
+       #         area = cv2.contourArea(c)
+       #         cv2.drawContours(eye, contours, contourIdx=-1, color=(0, 0, 255), thickness=2)
+
+        right_mouth1 = landmarks.part(54)
+        right_mouth2 = landmarks.part(55)
+        right_mouth3 = landmarks.part(65)
+        right_mouth4 = landmarks.part(56)
+
+        left_mouth1 = landmarks.part(50)
+        left_mouth2 = landmarks.part(49)
+        left_mouth3 = landmarks.part(61)
+        left_mouth4 = landmarks.part(60)
+
+        right_mouth_region = np.array([(landmarks.part(54).y),
+                                       (landmarks.part(55).y),
+                                       (landmarks.part(65).y),
+                                       (landmarks.part(56).y)],
+                                      np.int32)
+
+        left_mouth_region = np.array([(landmarks.part(50).y),
+                                      (landmarks.part(49).y),
+                                      (landmarks.part(61).y),
+                                      (landmarks.part(60).y)],
+                                     np.int32)
+
+        if control_mouth_length == 0:
+            control_mouth_length = math.sqrt(((landmarks.part(49).y - landmarks.part(55).y) ** 2) + (
+                        (landmarks.part(55).x - landmarks.part(55).x) ** 2))
+
+        current_mouth_length = math.sqrt(
+            ((landmarks.part(49).y - landmarks.part(55).y) ** 2) + ((landmarks.part(55).x - landmarks.part(55).x) ** 2))
+
+        mouth_distance_increase = ((current_mouth_length - control_mouth_length) / control_mouth_length) * 100.0
+        #print('increase: ', mouth_distance_increase)
+
+        eyebrow_ratio, upper_eyebrow_ratio = get_eyebrow_ratio([22, 23, 28, 21, 24], landmarks)
+
+        #print('ratio: ', upper_eyebrow_ratio)
+
+       # if eyebrow_ratio < 0.72:
+        #    cv2.putText(frame, 'frown', (50, 150), font, 3, (255, 0, 0))
+        #elif eyebrow_ratio > 0.765 or upper_eyebrow_ratio > 0.705:
+         #   cv2.putText(frame, 'raised', (50, 150), font, 3, (255, 0, 0))
+        #else:
+         #   cv2.putText(frame, 'normal', (50, 150), font, 3, (255, 0, 0))
 
     cv2.imshow('frame', frame)
 
     # save on pressing 'y'
-    if running or capture:
-        running = False
+    if (cv2.waitKey(1) & 0xFF == ord('y')) or capture:
+        # running = False
         if currentframes < numframes:
             capture = True
             currentframes = currentframes + 1
@@ -253,6 +296,8 @@ while True:
             face_points.append('blink')
         else:
             face_points.append('no blink')
+        face_points.append(cheek_difference)
+        print('difference: ', cheek_difference)
 
         with open('test.csv', 'a', newline='') as myFile:
             wr = csv.writer(myFile)
@@ -260,9 +305,8 @@ while True:
 
         face_points = []
 
-    #break program on press of 'Esc'
+    # break program on press of 'Esc'
     if cv2.waitKey(1) == 27:
         cap.release()
         cv2.destroyAllWindows()
         break
-
