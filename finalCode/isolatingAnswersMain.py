824285dace45a2f1d79d60bc2f2de3b14b4677ea
Lauren <C16452764@mydit.ie>
2020-04-08 19:37:58 +0100

    cleaned up video manager code

--- finalCode/isolatingAnswersMain.py
+++ finalCode/isolatingAnswersMain.py
@@ -1,45 +1,42 @@
 import cv2
-from cropImage2 import get_cropped_image2
+from trackEvent import event_detection
 
-cap = cv2.VideoCapture('final6.mp4')
+cap = cv2.VideoCapture('kyle.mp4')
 fourcc = cv2.VideoWriter_fourcc(*'mp4v')
 
 writeVideo = False
 count = 1
 videos = []
 
-out = cv2.VideoWriter('Answers\\Answer' + str(count) + '.mp4', fourcc, 20.0, (int(cap.get(3)), int(cap.get(4))))
+out = cv2.VideoWriter('Answers\\laura\\Answer' + str(count) + '.mp4', fourcc, 20.0, (int(cap.get(3)), int(cap.get(4))))
 
 while True:
 
     _, frame = cap.read()
     #frame = cv2.resize(frame, (0,0), fx=1.1, fy=1.1)
-    frame = cv2.rotate(frame, cv2.ROTATE_180)
-
-    frame = cv2.resize(frame, (0, 0), fx=0.4, fy=0.4)
+    #frame = cv2.rotate(frame, cv2.ROTATE_180)
+    #frame = cv2.resize(frame, (0, 0), fx=0.4, fy=0.4)
 
     if cap.isOpened():
         fps = cap.get(cv2.CAP_PROP_FPS)
-        # can change to get(5) - will get
         size = (int(cap.get(cv2.CAP_PROP_FRAME_WIDTH)),
                 int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT)))
 
-    cropped = get_cropped_image2(frame)
+    cropped = event_detection(frame)
 
     if cropped:
         videos.append(frame)
         writeVideo = True
     else:
-        if writeVideo and len(videos) > 10:
-            out = cv2.VideoWriter('Answers\\Answer' + str(count) + '.mp4', fourcc, 20.0,
+        if writeVideo and len(videos) > 20:
+            out = cv2.VideoWriter('Answers\\laura\\Answer' + str(count) + '.mp4', fourcc, 20.0,
                                   (int(cap.get(3)), int(cap.get(4))))
 
             for images in videos:
                # images = cv2.rotate(images, cv2.ROTATE_180)
-
-                images = cv2.resize(images, (0, 0), fx=2.5, fy=2.5)
+               # images = cv2.resize(images, (0, 0), fx=2.5, fy=2.5)
                 out.write(images)
-            print('************************  ', count)
+            print('Successfully created video for Answer ', count)
             videos = []
             count = count + 1
             writeVideo = False
@@ -51,7 +48,3 @@ while True:
         cap.release()
         cv2.destroyAllWindows()
         break
-
-cap.release()
-out.release()
-cv2.destroyAllWindows()
--- finalCode/trackEvent.py
+++ finalCode/trackEvent.py
@@ -1,55 +1,42 @@
 import cv2
 
-def get_cropped_image2(img):
+
+#function to isolate the laptop screen from original image in order for detection of triangle occurence
+def event_detection(img):
     frame_changed = False
 
     g = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
     gray = cv2.bilateralFilter(g, 1, 10, 120)
-
     edges = cv2.Canny(gray, 10, 250)
-
     kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (7, 7))
-
     closed = cv2.morphologyEx(edges, cv2.MORPH_CLOSE, kernel)
-
-    cv2.imshow('closed', closed)
 
     contours, h = cv2.findContours(closed, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
     for cont in contours:
-
         if cv2.contourArea(cont) > 5000:
-
             arc_len = cv2.arcLength(cont, True)
-
             approx = cv2.approxPolyDP(cont, 0.1 * arc_len, True)
 
             if len(approx) == 4:
-                cv2.drawContours(img, [approx], -1, (255, 0, 0), 2)
-                # new_img = img[approx[0, 0, 1]:approx[2, 0, 1], approx[0, 0, 0]:approx[2, 0, 0]]
                 x, y, w, h = cv2.boundingRect(approx)
                 new_img = img[y:y + h, x:x + w]
-                gray2 = cv2.cvtColor(new_img, cv2.COLOR_BGR2GRAY)
 
-                gray2 = cv2.bilateralFilter(gray2, 1, 10, 120)
+                grayCropped = cv2.cvtColor(new_img, cv2.COLOR_BGR2GRAY)
+                grayCropped = cv2.bilateralFilter(grayCropped, 1, 10, 120)
 
-                edges2 = cv2.Canny(gray2, 10, 250)
-
+                edges2 = cv2.Canny(grayCropped, 10, 250)
                 kernel2 = cv2.getStructuringElement(cv2.MORPH_RECT, (7, 7))
-
                 closed2 = cv2.morphologyEx(edges2, cv2.MORPH_CLOSE, kernel2)
 
                 contours, hierarchy = cv2.findContours(closed2, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-                # print(contours)
                 for c in contours:
-
                     peri = cv2.arcLength(c, True)
                     approx = cv2.approxPolyDP(c, 0.04 * peri, True)
 
                     # check to see if contours have been found
                     if len(approx) == 3:
-                        cv2.drawContours(new_img, [approx], -1, (255, 0, 0), 2)
                         frame_changed = True
 
     return frame_changed
